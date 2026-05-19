130ed1da9df60bec3ccf9be772656af82272182a
Amruta Tekale <atekale046@gmail.com>
2026-05-19 09:07:00 +0530

    Changed some files

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { genTicket, sum } from "./helper";
 import Ticket from "./Ticket";
+import "./Button.css";
 
 export default function Lottery({n = 3, winCondition}) {
     let [ticket, setTicket] = useState(genTicket(n));
@@ -14,7 +15,7 @@ export default function Lottery({n = 3, winCondition}) {
         <div>
             <h1>Lottery Game!</h1>
                 <Ticket ticket={ticket}/>
-            <button onClick={buyTicket}>Buy new Ticket</button>
+            <button onClick={buyTicket} className="Button">Buy new Ticket</button>
             <h3>{isWinning && "Congratulations, you won!"}</h3>
         </div>
     );
--- src/Ticket.jsx
+++ src/Ticket.jsx
@@ -5,13 +5,9 @@ export default function Ticket({ticket}){
     return(
         <div className="Ticket">
             <p>Ticket</p>
-
-            <div className="numbers">
                 {ticket.map((num, idx) => (
                     <TicketNum num={num} key={idx} />
                 ))}
-            </div>
-
         </div>
     );
 }
